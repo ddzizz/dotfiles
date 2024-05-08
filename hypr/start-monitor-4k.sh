67b12442059f6ea8057df3389c7c9ac8f56c7af3
ddzizz <ding_r@foxmail.com>
2024-05-08 12:00:54 +0800

    merge vmware dotfiles

--- hypr/start-monitor-4k.sh
+++ hypr/start-monitor-4k.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cp ~/dotfiles/hypr/config/monitor-macbook-air.conf ~/dotfiles/hypr/config/monitor.conf
+cp ~/dotfiles/hypr/config/monitor-4k.conf ~/dotfiles/hypr/config/monitor.conf
 #export WLR_NO_HARDWARE_CURSORS=1
 #export WLR_RENDERER_ALLOW_SOFTWARE=1
 exec Hyprland
