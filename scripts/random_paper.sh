2ae63e2b82ec371e17207c53bd5921fa783400ca
ddzizz <ding_r@foxmail.com>
2024-05-10 20:49:57 +0800

    wallock not work on vmware, revert to hyprpapper

--- scripts/random_paper.sh
+++ scripts/random_paper.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# random_wallpaper() {
-# 	wallpaper_dir="$HOME/dotfiles/wallpapers"
-#
-# 	files=($wallpaper_dir/*)
-#
-# 	random_file=("${files[RANDOM % ${#files[@]}]}")
-#
-# 	echo "set wallpaper $random_file"
-# 	hyprctl hyprpaper preload "$random_file"
-# 	hyprctl hyprpaper wallpaper "eDP-1, $random_file"
-# 	hyprctl hyprpaper wallpaper "Virtual-1, $random_file"
-# 	hyprctl hyprpaper unload all
-# }
-#
-# random_wallpaper
-wallock -o next
+random_wallpaper() {
+	wallpaper_dir="$HOME/dotfiles/wallpapers"
+
+	files=($wallpaper_dir/*)
+
+	random_file=("${files[RANDOM % ${#files[@]}]}")
+
+	echo "set wallpaper $random_file"
+	hyprctl hyprpaper preload "$random_file"
+	hyprctl hyprpaper wallpaper ",$random_file"
+	# hyprctl hyprpaper wallpaper "eDP-1, $random_file"
+	# hyprctl hyprpaper wallpaper "Virtual-1, $random_file"
+	hyprctl hyprpaper unload unused
+}
+
+random_wallpaper
+# wallock -o next
