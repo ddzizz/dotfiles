3737c12ceb38b006473614fbf51bdf644cd92813
ddzizz <ding_r@foxmail.com>
2024-08-29 17:56:45 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,4 +7,8 @@ sudo pacman -S wqy-microhei
 sudo pacman -S wqy-zenhei
 fc-cache -fv
 
+sudo pacman -S gcc make cmake clang git neovim zip unzip man-db
+sudo pacman -S go rust  dotnet-sdk-7.0 dotnet-sdk-8.0
+sudo pacman -S hyprland hyprpaper
+sudo pacman -S foot neofetch waybar wofi rofi-wayland wl-clipboard wlroots wlr-randr wlogout
 sudo pacman -S yazi ffmpegthumbnailer p7zip jq poppler fd ripgrep fzf zoxide imagemagick 
