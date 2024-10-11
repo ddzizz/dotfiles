564af98df33dc5feb9efc39f84c33a9b707d4157
ddzizz <ding_r@foxmail.com>
2024-10-11 16:29:01 +0800

    use swww instead hyprpaper

--- scripts/launch-vmware.sh
+++ scripts/launch-vmware.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cp ~/dotfiles/hypr/config/monitor-4k.conf ~/dotfiles/hypr/config/monitor.conf
-~/dotfiles/scripts/random_launch_paper.sh
+~/dotfiles/scripts/random_paper.sh
+# ~/dotfiles/scripts/random_launch_paper.sh
 #export WLR_NO_HARDWARE_CURSORS=1
 #export WLR_RENDERER_ALLOW_SOFTWARE=1
 exec Hyprland
--- scripts/random_paper.sh
+++ scripts/random_paper.sh
@@ -16,5 +16,18 @@ random_wallpaper() {
 	hyprctl hyprpaper unload unused
 }
 
-random_wallpaper
+random_wallpaper_with_swww() {
+
+	wallpaper_dir="$HOME/dotfiles/wallpapers"
+
+	files=($wallpaper_dir/*)
+
+	random_file=("${files[RANDOM % ${#files[@]}]}")
+
+	echo "set wallpaper $random_file"
+	swww img "$random_file"
+}
+
+random_wallpaper_with_swww
+# random_wallpaper
 # wallock -o next
